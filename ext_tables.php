bff2ac015efd2632424f769274240c44c353e727
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-11-18 17:21:53 +0000

    "Added plugin to \"New content element\" wizard"
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/comments/trunk@7259 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -39,7 +39,7 @@ function tx_comments_makeTempFlexFormDS() {
 	return 'FILE:' . substr($ffFileName, strlen(PATH_site));
 }
 
-if (!defined('TYPO3_MODE'))  die ('Access denied.');
+if (!defined('TYPO3_MODE')) die('Access denied.');
 
 // Add static files for plugins
 t3lib_extMgm::addStaticFile($_EXTKEY, 'pi1/static/', 'Commenting system');
@@ -84,4 +84,7 @@ t3lib_extMgm::allowTableOnStandardPages('tx_comments_comments');
 t3lib_extMgm::addToInsertRecords('tx_comments_comments');
 t3lib_extMgm::addLLrefForTCAdescr('tx_comments_comments', 'EXT:comments/locallang_csh.php');
 
+if (TYPO3_MODE == 'BE') {
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_comments_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_comments_pi1_wizicon.php';
+}
 ?>

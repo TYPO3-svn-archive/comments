11378d256988f66871eefd10f6b2bad1f24b3708
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-01-05 15:52:37 +0000

    Changed name and company
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/comments/trunk@7829 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "comments"
 #
-# Auto generated 06-12-2007 12:52
+# Auto generated 14-12-2007 21:46
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Commenting system',
 	'description' => 'Adds commenting for pages or virtually any record visible in frontend',
 	'category' => 'plugin',
-	'author' => 'Dmitry Dulepov',
+	'author' => 'Dmitry Dulepov [netcreators]',
 	'author_email' => 'dmitry@typo3.org',
 	'shy' => '',
 	'dependencies' => '',
@@ -28,8 +28,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author_company' => 'http://typo3bloke.net/',
-	'version' => '1.0.5',
+	'author_company' => 'Netcreators BV',
+	'version' => '1.0.6',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"7278";s:32:"class.tx_comments_cms_layout.php";s:4:"58cf";s:25:"class.tx_comments_eID.php";s:4:"aa34";s:29:"class.tx_comments_tcemain.php";s:4:"fa5d";s:12:"ext_icon.gif";s:4:"7edf";s:17:"ext_localconf.php";s:4:"d5ce";s:14:"ext_tables.php";s:4:"dc92";s:14:"ext_tables.sql";s:4:"59e7";s:22:"flexform_functions.php";s:4:"4db3";s:17:"icon_comments.gif";s:4:"07a5";s:30:"icon_comments_not_approved.gif";s:4:"1d20";s:17:"locallang_csh.xml";s:4:"616e";s:16:"locallang_db.xml";s:4:"fd6d";s:17:"locallang_eID.xml";s:4:"ed77";s:7:"tca.php";s:4:"1e95";s:12:"doc/TODO.txt";s:4:"27a3";s:14:"doc/manual.sxw";s:4:"3562";s:14:"pi1/ce_wiz.gif";s:4:"c787";s:29:"pi1/class.tx_comments_pi1.php";s:4:"5eee";s:37:"pi1/class.tx_comments_pi1_wizicon.php";s:4:"28e3";s:19:"pi1/flexform_ds.xml";s:4:"a146";s:28:"pi1/flexform_ds_advanced.xml";s:4:"b230";s:27:"pi1/flexform_ds_general.xml";s:4:"4c38";s:31:"pi1/flexform_ds_spamprotect.xml";s:4:"5270";s:17:"pi1/locallang.xml";s:4:"64d1";s:24:"pi1/static/constants.txt";s:4:"c428";s:20:"pi1/static/setup.txt";s:4:"1ee4";s:25:"res/commenting-closed.gif";s:4:"10c8";s:13:"res/email.txt";s:4:"12cf";s:11:"res/pi1.css";s:4:"e5c1";s:10:"res/pi1.js";s:4:"bd93";s:21:"res/pi1_template.html";s:4:"887f";}',
+	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"7278";s:32:"class.tx_comments_cms_layout.php";s:4:"58cf";s:25:"class.tx_comments_eID.php";s:4:"aa34";s:29:"class.tx_comments_tcemain.php";s:4:"fa5d";s:12:"ext_icon.gif";s:4:"7edf";s:17:"ext_localconf.php";s:4:"d5ce";s:14:"ext_tables.php";s:4:"dc92";s:14:"ext_tables.sql";s:4:"59e7";s:22:"flexform_functions.php";s:4:"4db3";s:17:"icon_comments.gif";s:4:"07a5";s:30:"icon_comments_not_approved.gif";s:4:"1d20";s:17:"locallang_csh.xml";s:4:"616e";s:16:"locallang_db.xml";s:4:"fd6d";s:17:"locallang_eID.xml";s:4:"ed77";s:7:"tca.php";s:4:"1e95";s:12:"doc/TODO.txt";s:4:"27a3";s:14:"doc/manual.sxw";s:4:"3562";s:14:"pi1/ce_wiz.gif";s:4:"c787";s:29:"pi1/class.tx_comments_pi1.php";s:4:"927e";s:37:"pi1/class.tx_comments_pi1_wizicon.php";s:4:"28e3";s:19:"pi1/flexform_ds.xml";s:4:"a146";s:28:"pi1/flexform_ds_advanced.xml";s:4:"b230";s:27:"pi1/flexform_ds_general.xml";s:4:"4c38";s:31:"pi1/flexform_ds_spamprotect.xml";s:4:"5270";s:17:"pi1/locallang.xml";s:4:"64d1";s:24:"pi1/static/constants.txt";s:4:"c428";s:20:"pi1/static/setup.txt";s:4:"1ee4";s:25:"res/commenting-closed.gif";s:4:"10c8";s:13:"res/email.txt";s:4:"12cf";s:11:"res/pi1.css";s:4:"e5c1";s:10:"res/pi1.js";s:4:"bd93";s:21:"res/pi1_template.html";s:4:"3743";}',
 	'suggests' => array(
 	),
 );

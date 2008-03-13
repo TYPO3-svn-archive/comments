aca434516afbba0be22d9760e92a354b9d36daa9
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-03-13 15:08:21 +0000

    1.3.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/comments/trunk@8575 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "comments"
 #
-# Auto generated 18-02-2008 14:29
+# Auto generated 13-03-2008 17:06
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'Netcreators BV',
-	'version' => '1.3.1',
+	'version' => '1.3.2',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:35:{s:9:"ChangeLog";s:4:"25f6";s:32:"class.tx_comments_cms_layout.php";s:4:"58cf";s:25:"class.tx_comments_eID.php";s:4:"b62c";s:29:"class.tx_comments_tcemain.php";s:4:"1c19";s:28:"class.tx_comments_ttnews.php";s:4:"a8db";s:12:"ext_icon.gif";s:4:"7edf";s:17:"ext_localconf.php";s:4:"547a";s:14:"ext_tables.php";s:4:"dc92";s:14:"ext_tables.sql";s:4:"59e7";s:22:"flexform_functions.php";s:4:"4db3";s:17:"icon_comments.gif";s:4:"07a5";s:30:"icon_comments_not_approved.gif";s:4:"1d20";s:17:"locallang_csh.xml";s:4:"616e";s:16:"locallang_db.xml";s:4:"fd6d";s:17:"locallang_eID.xml";s:4:"ed77";s:19:"locallang_hooks.xml";s:4:"7e29";s:7:"tca.php";s:4:"1e95";s:15:"csh/captcha.png";s:4:"41a5";s:14:"doc/manual.sxw";s:4:"474a";s:14:"pi1/ce_wiz.gif";s:4:"c787";s:29:"pi1/class.tx_comments_pi1.php";s:4:"7fd2";s:37:"pi1/class.tx_comments_pi1_wizicon.php";s:4:"28e3";s:19:"pi1/flexform_ds.xml";s:4:"0040";s:28:"pi1/flexform_ds_advanced.xml";s:4:"9f33";s:27:"pi1/flexform_ds_general.xml";s:4:"a1c8";s:31:"pi1/flexform_ds_spamprotect.xml";s:4:"2520";s:17:"pi1/locallang.xml";s:4:"8b5d";s:21:"pi1/locallang_csh.xml";s:4:"1cb5";s:24:"pi1/static/constants.txt";s:4:"0c25";s:20:"pi1/static/setup.txt";s:4:"f676";s:25:"res/commenting-closed.gif";s:4:"bd93";s:13:"res/email.txt";s:4:"12cf";s:11:"res/pi1.css";s:4:"ffe7";s:10:"res/pi1.js";s:4:"bd93";s:21:"res/pi1_template.html";s:4:"fbf8";}',
+	'_md5_values_when_last_written' => 'a:37:{s:9:"ChangeLog";s:4:"33c6";s:32:"class.tx_comments_cms_layout.php";s:4:"58cf";s:25:"class.tx_comments_eID.php";s:4:"b62c";s:29:"class.tx_comments_tcemain.php";s:4:"16cd";s:28:"class.tx_comments_ttnews.php";s:4:"6ca3";s:12:"ext_icon.gif";s:4:"7edf";s:17:"ext_localconf.php";s:4:"547a";s:14:"ext_tables.php";s:4:"96a1";s:14:"ext_tables.sql";s:4:"0077";s:22:"flexform_functions.php";s:4:"4db3";s:17:"icon_comments.gif";s:4:"07a5";s:30:"icon_comments_not_approved.gif";s:4:"1d20";s:15:"icon_urllog.gif";s:4:"0ad4";s:17:"locallang_csh.xml";s:4:"cfb6";s:16:"locallang_db.xml";s:4:"256e";s:17:"locallang_eID.xml";s:4:"ed77";s:19:"locallang_hooks.xml";s:4:"7e29";s:7:"tca.php";s:4:"f42b";s:15:"csh/captcha.png";s:4:"41a5";s:14:"doc/manual.sxw";s:4:"6033";s:14:"pi1/ce_wiz.gif";s:4:"c787";s:29:"pi1/class.tx_comments_pi1.php";s:4:"7789";s:34:"pi1/class.tx_comments_pi1.php.orig";s:4:"d976";s:37:"pi1/class.tx_comments_pi1_wizicon.php";s:4:"28e3";s:19:"pi1/flexform_ds.xml";s:4:"0040";s:28:"pi1/flexform_ds_advanced.xml";s:4:"374f";s:27:"pi1/flexform_ds_general.xml";s:4:"4c0c";s:31:"pi1/flexform_ds_spamprotect.xml";s:4:"2520";s:17:"pi1/locallang.xml";s:4:"536a";s:21:"pi1/locallang_csh.xml";s:4:"07b7";s:24:"pi1/static/constants.txt";s:4:"cad0";s:20:"pi1/static/setup.txt";s:4:"373d";s:25:"res/commenting-closed.gif";s:4:"bd93";s:13:"res/email.txt";s:4:"12cf";s:11:"res/pi1.css";s:4:"ffe7";s:10:"res/pi1.js";s:4:"bd93";s:21:"res/pi1_template.html";s:4:"b39e";}',
 	'suggests' => array(
 	),
 );
